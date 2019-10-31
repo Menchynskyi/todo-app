49558ab91c4f85a898c40935eaca5d8be6073100
Menchynskyi <the.mngs@yahoo.com>
2019-10-31 22:45:27 +0200

    Updates

---
+++ precache-manifest.5a6032268c98d3da215c8a450cd20e24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eb1831a4caf9b86db723b7e74c8e631",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "d6bfb9e2ce084427dd66",
+    "url": "/todo-app/static/css/main.03eac1e0.chunk.css"
+  },
+  {
+    "revision": "bf30aed112e05069181f",
+    "url": "/todo-app/static/js/2.49dc4c9b.chunk.js"
+  },
+  {
+    "revision": "d6bfb9e2ce084427dd66",
+    "url": "/todo-app/static/js/main.16162cb3.chunk.js"
+  },
+  {
+    "revision": "52d3efa2cb86a8d220e2",
+    "url": "/todo-app/static/js/runtime-main.fc9f2c6d.js"
+  }
+]);
